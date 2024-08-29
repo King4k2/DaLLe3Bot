38c940dbf10811652d78c40a642566b001fd6ffa
Nikita <laim51709@gmail.com>
2024-08-29 13:52:09 +0400

    Release 1.1

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 API_KEY = ""
-proxy_url = "http://hvrNfVwr:Ld5ZnE9K@212.52.6.52:64384"
+proxy_url = ""
 # {type of proxy}://{username}:{password}@{ip}:{port}
 # example: socks5://hvbNeVwe:Td5zNE1K@215.52.1.52:61383
 
